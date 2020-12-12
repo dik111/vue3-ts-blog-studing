a514be3e6f9f62a89b633516d9fb5a82e1d21358
yuweiZhan <195812208@qq.com>
2020-12-12 17:35:03 +0800

    :sparkles: 11vue3-tab切换和单元测试开始学习

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,21 +1,34 @@
 <template>
   <nav class="is-primary panel">
    <p class="panel-tabs">
-     <a data-test="period" v-for="(period,index) in periods" :key="index">{{period}}</a>
+     <a
+         @click="setPeriod(period)"
+         :class="[period === selectedPeriod ? 'is-active':'']"
+         data-test="period" v-for="(period,index) in periods" :key="index">{{period}}</a>
    </p>
   </nav>
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue'
+import {defineComponent,ref} from 'vue'
 import {Period} from "@/types";
 
 export default defineComponent({
   name: "Timeline",
   setup() {
     const periods:Period[] = ["今天", "本周", "本月"]
+    const selectedPeriod = ref<Period>("今天")
 
-    return {periods}
+    // tab切换
+    const setPeriod = (period:Period) =>{
+      selectedPeriod.value = period;
+    }
+
+    return {
+      periods,
+      selectedPeriod,
+      setPeriod
+    }
   }
 })
 </script>
--- tests/unit/Timeline.spec.ts
+++ tests/unit/Timeline.spec.ts
@@ -1,13 +1,25 @@
 import { mount } from '@vue/test-utils'
 import Timeline from '@/components/Timeline.vue'
+import {nextTick} from 'vue'
 
 describe('Timeline.vue', () => {
   it('测试3个a标签的功能', () => {
     const wrapper = mount(Timeline, {})
-    // console.log(wrapper.html());
-    // expect(1).toBe(2)
-    // const periods = wrapper.findAll("a")
-    // expect(periods.length).toBe(3)
     expect(wrapper.findAll("[data-test='period']")).toHaveLength(3)
+  })
+  it('测试a标签的事件功能', async () => {
+    const wrapper = mount(Timeline, {})
+    const $today =  wrapper.findAll("[data-test='period']")[0]
+    expect($today.classes()).toContain("is-active")
+
+    const $thisWeek =  wrapper.findAll("[data-test='period']")[1]
+    await $thisWeek.trigger("click")
+
+    // dom更新完毕
+    // await nextTick()
+    expect($today.classes()).not.toContain("is-active")
+    expect($thisWeek.classes()).toContain("is-active")
+
+    const $thisMonth =  wrapper.findAll("[data-test='period']")[2]
   })
 })
