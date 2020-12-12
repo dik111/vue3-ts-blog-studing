cdecd94ad0ae406f0ad2a830c8d9f41e9be36a8d
yuweiZhan <195812208@qq.com>
2020-12-12 18:00:58 +0800

    :sparkles: 12vue3-模拟数据

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -6,12 +6,20 @@
          :class="[period === selectedPeriod ? 'is-active':'']"
          data-test="period" v-for="(period,index) in periods" :key="index">{{period}}</a>
    </p>
+    <a v-for="(post) in posts " :key="post.id" class="panel-block">
+      <div>
+        <a>{{post.title}}</a>
+        <div>{{post.created.format("yyyy-MM-dd")}}</div>
+      </div>
+    </a>
   </nav>
 </template>
 
 <script lang="ts">
-import {defineComponent,ref} from 'vue'
-import {Period} from "@/types";
+import {defineComponent,ref,computed} from 'vue'
+import {Period,Post} from "@/types";
+import {todayPost,thisWeek,thisMonth} from "@/mock";
+import moment from "moment";
 
 export default defineComponent({
   name: "Timeline",
@@ -24,10 +32,31 @@ export default defineComponent({
       selectedPeriod.value = period;
     }
 
+    // 展示数据
+    // const posts:Post[] = [todayPost,thisWeek,thisMonth]
+    const posts = computed(() =>[todayPost,thisWeek,thisMonth].filter(post =>{
+      if (selectedPeriod.value === "今天" && post.created.isAfter(moment().subtract(1,"day"))){
+        return true
+      }else{
+        false
+      }
+      if (selectedPeriod.value === "本周" && post.created.isAfter(moment().subtract(7,"days"))){
+        return true
+      }else{
+        false
+      }
+      if (selectedPeriod.value === "本月" && post.created.isAfter(moment().subtract(1,"month"))){
+        return true
+      }else{
+        false
+      }
+    }))
+
     return {
       periods,
       selectedPeriod,
-      setPeriod
+      setPeriod,
+      posts
     }
   }
 })
---
+++ src/mock/index.ts
@@ -0,0 +1,25 @@
+import moment from 'moment'
+import {Post} from "@/types";
+export const basePost:Post = {
+    id:1,
+    title:"米修在线",
+    markdown:"课程列表",
+    html:"<p>课程列表</p>",
+    authorId:1,
+    created:moment()
+}
+
+export const todayPost:Post = {
+    ...basePost,
+    title: "今天"
+}
+export const thisWeek:Post = {
+    ...basePost,
+    title: "本周",
+    created: moment().subtract(2,"days")
+}
+export const thisMonth:Post = {
+    ...basePost,
+    title: "本月",
+    created: moment().subtract(2,"weeks")
+}
