48b23b7ece6e6251522a12e54c80c470bda153dc
yuweiZhan <195812208@qq.com>
2020-12-12 15:45:46 +0800

    :tada: init project

---
+++ src/components/Timeline.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="is-primary panel">
+   <p class="panel-tabs">
+     <a v-for="(period,index) in periods" :key="index">{{period}}</a>
+   </p>
+  </nav>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+import {Period} from "@/types";
+
+export default defineComponent({
+  name: "Timeline",
+  setup() {
+    const periods:Period[] = ["今天", "本周", "本月"]
+    return {periods}
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
